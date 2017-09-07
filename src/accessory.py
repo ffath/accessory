fde89f9a1def7a9ff4742799e13c5cc116d382eb
François Fath <francois.fath@free.fr>
2017-09-07 15:59:35 +0200

    Add FFWD & RWND

--- src/accessory.py
+++ src/accessory.py
@@ -15,7 +15,7 @@ HID_REPORT_DESCRIPTOR = array.array('B', [
     0x06, 0x0C, 0x00,   # Usage consumer
     0x09, 0x01,         # Usage
     0xA1, 0x01,         # Collection
-    0x85, 0x03,         # Report ID (3)
+    0x85, HID_REPORT_ID,  # Report ID (3)
     0x25, 0x01,         # Logical maximum (1)
     0x15, 0x00,         # Logical minimum (0)
     0x75, 0x01,         # Report size (1)
@@ -29,19 +29,19 @@ HID_REPORT_DESCRIPTOR = array.array('B', [
     0x0A, 0xEA, 0x00,   # Usage Vol Dec
     0x95, 0x08,         # Report count (8)
     0x81, 0x02,         # Input
-    0x0A, 0x83, 0x01,   # Usage ???
-    0x0A, 0x8A, 0x01,   # Usage ???
-    0x0A, 0x92, 0x01,   # Usage ???
-    0x0A, 0x94, 0x01,   # Usage ???
+    0x0A, 0x83, 0x01,   # Usage Config ?
+    0x0A, 0x8A, 0x01,   # Usage Mail ?
+    0x0A, 0x92, 0x01,   # Usage Calc ?
+    0x0A, 0x94, 0x01,   # Usage File ?
     0x0A, 0x21, 0x02,   # Usage Search
-    0x0A, 0x23, 0x02,   # Usage ???
+    0x0A, 0x23, 0x02,   # Usage Browser Home ?
     0x0A, 0x24, 0x02,   # Usage Browser Back
     0x0A, 0x25, 0x02,   # Usage Browser Forward
     0x95, 0x08,         # Report count (8)
     0x81, 0x02,         # Input
     0x0A, 0x26, 0x02,   # Usage Browser Stop
     0x0A, 0x27, 0x02,   # Usage Browser Refresh
-    0x0A, 0x2A, 0x02,   # Usage ???
+    0x0A, 0x2A, 0x02,   # Usage Browser Bookmarks ?
     0x0A, 0xB3, 0x00,   # Usage Fast Forward
     0x0A, 0xB4, 0x00,   # Usage Rewind
     0x95, 0x05,         # Report count (5)
@@ -52,15 +52,17 @@ HID_REPORT_DESCRIPTOR = array.array('B', [
 ])
 
 # Keys
-KEY_NONE = 0x00
-KEY_NEXT = 0x01
-KEY_PREV = 0x02
-KEY_STOP = 0x04
-KEY_EJECT = 0x08
-KEY_PLAY_PAUSE = 0x10
-KEY_MUTE = 0x20
-KEY_VOL_INC = 0x40
-KEY_VOL_DEC = 0x80
+KEY_NONE = []
+KEY_NEXT = [0, 0x01]
+KEY_PREV = [0, 0x02]
+KEY_STOP = [0, 0x04]
+KEY_EJECT = [0, 0x08]
+KEY_PLAY_PAUSE = [0, 0x10]
+KEY_MUTE = [0, 0x20]
+KEY_VOL_INC = [0, 0x40]
+KEY_VOL_DEC = [0, 0x80]
+KEY_FAST_FORWARD = [2, 0x08]
+KEY_REWIND = [2, 0x10]
 
 
 def set_accessory_mode(idVendor, idProduct, silent=True):
@@ -113,13 +115,18 @@ def set_accessory_mode(idVendor, idProduct, silent=True):
     return True
 
 
-def send_hid_event(dev, keys):
-    print "ACCESSORY_SEND_HID_EVENT ", dev.ctrl_transfer(
+def send_hid_event(dev, key):
+    hid_report = array.array('B', [HID_REPORT_ID, 0, 0, 0])
+
+    if key != KEY_NONE:
+        hid_report[key[0] + 1] = key[1]
+
+    print "ACCESSORY_SEND_HID_EVENT", hid_report, dev.ctrl_transfer(
         bmRequestType=0x40,
         bRequest=57,
         wValue=HID_ID,
         wIndex=0,
-        data_or_wLength=array.array('B', [0x03, keys, 0x00, 0x00]))
+        data_or_wLength=hid_report)
 
 
 def handle_accessory(idVendor, idProduct):
@@ -131,6 +138,8 @@ def handle_accessory(idVendor, idProduct):
         "mute": KEY_MUTE,
         "vol+": KEY_VOL_INC,
         "vol-": KEY_VOL_DEC,
+        "ffwd": KEY_FAST_FORWARD,
+        "rwnd": KEY_REWIND,
         "eject": KEY_EJECT,
     }
 
@@ -157,10 +166,10 @@ def handle_accessory(idVendor, idProduct):
     # process actions
     while True:
         try:
-            send_hid_event(dev=dev, keys=action[raw_input("> ")])
-            send_hid_event(dev=dev, keys=KEY_NONE)
+            send_hid_event(dev=dev, key=action[raw_input("> ")])
+            send_hid_event(dev=dev, key=KEY_NONE)
         except KeyError:
-            print "unknown action"
+            print "legit actions:", action
         except EOFError:
             print "EOFError"
 
